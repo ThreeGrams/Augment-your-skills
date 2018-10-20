44fbe30a2ae557fc3e13b61232867d9db9099858
Adam Barák <barak.adam94@gmail.com>
2018-10-21 00:23:37 +0200

    Image send upload

---
+++ AugmentYourSkills/Assets/_AugmentYourSkills/_Scripts/Networking/Client/ImageSenderClient.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+using AYS.Networking;
+using AYS.Camera;
+
+namespace AYS.Networking.Client {
+    public class ImageSenderClient : MonoBehaviour {
+        [SerializeField] private ServerConfiguration serverConfig = null;
+
+        private TcpClient tcpClient;
+    
+        private bool _isConnected;
+
+        // Start is called before the first frame update
+        void Start() {
+            tcpClient = new TcpClient();
+            tcpClient.Connect(serverConfig.ipAddr, serverConfig.port);
+            _isConnected = true;
+
+            StartCoroutine(SendCameraImagesToServer(1.0f));
+        }
+
+        IEnumerator SendCameraImagesToServer(float waitTime) {
+            DeviceCamera deviceCamera = DeviceCamera.instance;
+
+            if (!deviceCamera._cameraIsAvailable) {
+                yield return null;
+            }
+
+            WebCamTexture camera = deviceCamera._camera;
+            
+            while(camera.isPlaying) {
+                if (_isConnected) {
+                    NetworkingUtils.sendCurrentCameraFrameImage(tcpClient.GetStream(), camera);
+                }
+
+                yield return new WaitForSeconds(waitTime);
+            }
+
+        }
+    }
+}
