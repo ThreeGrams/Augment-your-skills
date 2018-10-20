63da5564c19bc3807c8c290039a825cb9b35d5de
Martin Graubner <graubnerm@email.cz>
2018-10-20 12:51:37 +0200

    created server scripts

---
+++ AugmentYourSkills/Assets/_AugmentYourSkills/_Scripts/Server/Server.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Server : MonoBehaviour
+{
+	private const int MAX_CLIENT_CONNECTIONS = 100;
+
+	[SerializeField] private ServerConfiguration _serverConfig = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
